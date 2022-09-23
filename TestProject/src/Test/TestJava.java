68f9bb4d0869f6119f77a1d66ebc834539f81b55
Kaysey N. <85714834+kay-ne@users.noreply.github.com>
2022-09-23 19:30:14 -0400

    Commit first and push test

--- TestProject/src/Test/TestJava.java
+++ TestProject/src/Test/TestJava.java
@@ -4,5 +4,6 @@ public class TestJava {
 	public static void main (String[]args)
 	{
 		System.out.println("Testing push.");
+		System.out.println("If this shows up after push, that means it worked.");
 	}
 }
